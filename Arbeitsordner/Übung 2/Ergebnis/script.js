fa0e9ec1c87e0a5a640a1193c35402d671bc0e43
MDuehr <maximiliandh@gmail.com>
2014-11-15 11:35:35 +0100

    ARBEITSORDNER
    LOS NIMM

---
+++ Arbeitsordner/Übung 2/Ergebnis/script.js
@@ -0,0 +1,11 @@
+function scrollFunction() {
+    var offset = window.pageYOffset;
+    if (offset > 5) {
+        document.getElementById("scrollNavBar2").style.visibility = "visible";
+    }
+    else {
+        document.getElementById("scrollNavBar2").style.visibility = "hidden"; 
+    }
+}
+
+window.onscroll = scrollFunction;
