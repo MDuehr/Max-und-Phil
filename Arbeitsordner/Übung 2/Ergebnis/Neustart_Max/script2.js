eb1240c77977fb0d7024fbcc39f99b34f93a3901
Philipp W <waackphilipp@gmail.com>
2014-11-21 15:43:59 +0100

    added script4 and updated content table for script2

--- Arbeitsordner/Übung 2/Ergebnis/Neustart_Max/script2.js
+++ Arbeitsordner/Übung 2/Ergebnis/Neustart_Max/script2.js
@@ -1,4 +1,18 @@
+$(document).ready(function(){
+    $('.hideSize').click(function(){
+    	$('#tableHidden tr td:nth-of-type(2), #tableHidden th:nth-child(2)').toggle();
+    })
+    $('.hideType').click(function(){
+    	$('#tableHidden tr td:nth-of-type(3), #tableHidden th:nth-child(3)').toggle();
+    })
+    $('.hideCreation').click(function(){
+    	$('#tableHidden tr td:nth-of-type(4), #tableHidden th:nth-child(4)').toggle();
+    })
+});
 
+
+
+/*
 var a = document.getElementsByClassName("hideSize");
 
 var b = document.getElementById("table");
@@ -9,3 +23,4 @@ function hideSize() {
 }
 
 a.onclick = hideSize;
+*/
