71af4c6ee1c5697947e6c105ad29cfb67f6de9ee
Philipp W <waackphilipp@gmail.com>
2014-11-21 15:30:06 +0100

    kacke

--- Arbeitsordner/Übung 2/Ergebnis/script2.js
+++ Arbeitsordner/Übung 2/Ergebnis/script2.js
@@ -1,11 +1,11 @@
-
-var a = document.getElementsByClassName("hideSize");
-
-var b = document.getElementById("table");
-
-function hideSize() {
-    'use strict';
-    b.style.visibility = "hidden";
-}
-
-a.onclick = hideSize;
+$(document).ready(function(){
+    $('.hideSize').click(function(){
+    	$('#tableHidden tr td:nth-of-type(2), #tableHidden th:nth-child(2)').toggle();
+    })
+    $('.hideType').click(function(){
+    	$('#tableHidden tr td:nth-of-type(3), #tableHidden th:nth-child(3)').toggle();
+    })
+    $('.hideCreation').click(function(){
+    	$('#tableHidden tr td:nth-of-type(4), #tableHidden th:nth-child(4)').toggle();
+    })
+});
