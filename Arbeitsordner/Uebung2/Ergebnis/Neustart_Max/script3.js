a2d8267c3050d5d95e7d1b8113b4492d02f028f7
Philipp W <waackphilipp@gmail.com>
2014-11-23 23:54:03 +0100

    json table in script3 umgeschrieben, bilder usw eingefügt

--- Arbeitsordner/Uebung2/Ergebnis/Neustart_Max/script3.js
+++ Arbeitsordner/Uebung2/Ergebnis/Neustart_Max/script3.js
@@ -1,3 +1,138 @@
+var json = [
+                  {
+                    "filename": "06-BLUEMARC - BLUEMARC-BLUEMARC_-_AGA_LIFE_2012.mp3",
+                    "size": "6",
+                    "type": "Audio",
+                    "creation": "10.03.2013",
+                    "action": "<a href='#Beispiel31'><i class='fa fa-eye'></i></a>" +
+                                "<a href='#Beispiel32'><i class='fa fa-mail-forward'></i></a>&#124;" + 
+                                "<a href='#Beispiel33'><i class='fa fa-pencil'></i></a>" +
+                                "<a href='#Beispiel34'><i class='fa fa-lock'></i></a>" +
+                                "<a href='#Beispiel35'><i class='fa fa-trash-o'></i></a>"
+                  },
+                  {
+                    "filename": "08-BLUEMARC - EWSKO_BLUEMARC-ROMANTIC WIND.mp3",
+                    "size": "5",
+                    "type": "Audio",
+                    "creation": "10.03.2013",
+                    "action": "<a href='#Beispiel31'><i class='fa fa-eye'></i></a>" +
+                                "<a href='#Beispiel32'><i class='fa fa-mail-forward'></i></a>&#124;" + 
+                                "<a href='#Beispiel33'><i class='fa fa-pencil'></i></a>" +
+                                "<a href='#Beispiel34'><i class='fa fa-lock'></i></a>" +
+                                "<a href='#Beispiel35'><i class='fa fa-trash-o'></i></a>"
+                  },
+                  {
+                    "filename": "<img alt='amsterdam' src='media/thumbnails/amsterdam.jpg'>  amsterdam.jpg",
+                    "size": "240",
+                    "type": "Bild",
+                    "creation": "10.03.2013",
+                    "action": "<a href='#Beispiel31'><i class='fa fa-eye'></i></a>" +
+                                "<a href='#Beispiel32'><i class='fa fa-mail-forward'></i></a>&#124;" + 
+                                "<a href='#Beispiel33'><i class='fa fa-pencil'></i></a>" +
+                                "<a href='#Beispiel34'><i class='fa fa-lock'></i></a>" +
+                                "<a href='#Beispiel35'><i class='fa fa-trash-o'></i></a>"
+                  },
+                  {
+                    "filename": "<img alt='beeren.jpg' src='media/thumbnails/beeren.jpg'/> beeren.jpg",
+                    "size": "556",
+                    "type": "Bild",
+                    "creation": "10.09.2013",
+                    "action": "<a href='#Beispiel31'><i class='fa fa-eye'></i></a>" +
+                                "<a href='#Beispiel32'><i class='fa fa-mail-forward'></i></a>&#124;" + 
+                                "<a href='#Beispiel33'><i class='fa fa-pencil'></i></a>" +
+                                "<a href='#Beispiel34'><i class='fa fa-lock'></i></a>" +
+                                "<a href='#Beispiel35'><i class='fa fa-trash-o'></i></a>"
+                  },
+                  {
+                    "filename": "<img alt='drachen.jpg' src='media/thumbnails/drachen.jpg'/>  drachen.jpg",
+                    "size": "311",
+                    "type": "Bild",
+                    "creation": "10.09.2013",
+                    "action": "<a href='#Beispiel31'><i class='fa fa-eye'></i></a>" +
+                                "<a href='#Beispiel32'><i class='fa fa-mail-forward'></i></a>&#124;" + 
+                                "<a href='#Beispiel33'><i class='fa fa-pencil'></i></a>" +
+                                "<a href='#Beispiel34'><i class='fa fa-lock'></i></a>" +
+                                "<a href='#Beispiel35'><i class='fa fa-trash-o'></i></a>"
+                  },
+                  {
+                    "filename": "leben.mpg",
+                    "size": "426",
+                    "type": "Video",
+                    "creation": "25.10.2014",
+                    "action": "<a href='#Beispiel31'><i class='fa fa-eye'></i></a>" +
+                                "<a href='#Beispiel32'><i class='fa fa-mail-forward'></i></a>&#124;" + 
+                                "<a href='#Beispiel33'><i class='fa fa-pencil'></i></a>" +
+                                "<a href='#Beispiel34'><i class='fa fa-lock'></i></a>" +
+                                "<a href='#Beispiel35'><i class='fa fa-trash-o'></i></a>"
+                  },
+                  {
+                    "filename": "mimetype.txt",
+                    "size": "2",
+                    "type": "Text",
+                    "creation": "03.09.2013",
+                    "action": "<a href='#Beispiel31'><i class='fa fa-eye'></i></a>" +
+                                "<a href='#Beispiel32'><i class='fa fa-mail-forward'></i></a>&#124;" + 
+                                "<a href='#Beispiel33'><i class='fa fa-pencil'></i></a>" +
+                                "<a href='#Beispiel34'><i class='fa fa-lock'></i></a>" +
+                                "<a href='#Beispiel35'><i class='fa fa-trash-o'></i></a>"
+                  },
+                  {
+                    "filename": "lizens_musik.txt",
+                    "size": "123",
+                    "type": "Text",
+                    "creation": "10.09.2013",
+                    "action": "<a href='#Beispiel31'><i class='fa fa-eye'></i></a>" +
+                                "<a href='#Beispiel32'><i class='fa fa-mail-forward'></i></a>&#124;" + 
+                                "<a href='#Beispiel33'><i class='fa fa-pencil'></i></a>" +
+                                "<a href='#Beispiel34'><i class='fa fa-lock'></i></a>" +
+                                "<a href='#Beispiel35'><i class='fa fa-trash-o'></i></a>"
+                  },
+                  {
+                    "filename": "rest_definition.txt",
+                    "size": "387",
+                    "type": "Text",
+                    "creation": "08.09.2013",
+                    "action": "<a href='#Beispiel31'><i class='fa fa-eye'></i></a>" +
+                                "<a href='#Beispiel32'><i class='fa fa-mail-forward'></i></a>&#124;" + 
+                                "<a href='#Beispiel33'><i class='fa fa-pencil'></i></a>" +
+                                "<a href='#Beispiel34'><i class='fa fa-lock'></i></a>" +
+                                "<a href='#Beispiel35'><i class='fa fa-trash-o'></i></a>"
+                  },
+                  {
+                    "filename": "readme.txt",
+                    "size": "54",
+                    "type": "Text",
+                    "creation": "24.12.2014",
+                    "action": "<a href='#Beispiel31'><i class='fa fa-eye'></i></a>" +
+                                "<a href='#Beispiel32'><i class='fa fa-mail-forward'></i></a>&#124;" + 
+                                "<a href='#Beispiel33'><i class='fa fa-pencil'></i></a>" +
+                                "<a href='#Beispiel34'><i class='fa fa-lock'></i></a>" +
+                                "<a href='#Beispiel35'><i class='fa fa-trash-o'></i></a>"
+                  }
+                  ];
+
+
+                    var tr;
+                    var trh;
+
+                    trh = $('<tr/>');
+                    trh.append("<th onclick='sortNames()'>Filename</th>");
+                    trh.append("<th onclick='sortSizes()'>Size</th>");
+                    trh.append("<th>Type</th>");
+                    trh.append("<th>Creation</th>");
+                    trh.append("<th>Actions</th>");
+                    $('#parseHead').append(trh);
+
+                    for (var i = 0; i < json.length; i++) {
+                        tr = $('<tr/>');
+                        tr.append("<td>" + json[i].filename + "</td>");
+                        tr.append("<td>" + json[i].size + "</td>");
+                        tr.append("<td>" + json[i].type + "</td>");
+                        tr.append("<td>" + json[i].creation + "</td>");
+                        tr.append("<td class='symboltd'>" + json[i].action + "</td>");
+                        $('#parseData').append(tr);
+                    }
+
 function sortSizes(){
             json.sort(function(a,b) { return parseFloat(b.size) - parseFloat(a.size) } );
 
@@ -11,7 +146,7 @@ function sortSizes(){
             tr.append("<td>" + json[i].size + "</td>");
             tr.append("<td>" + json[i].type + "</td>");
             tr.append("<td>" + json[i].creation + "</td>");
-            tr.append("<td>" + json[i].action + "</td>");
+            tr.append("<td  class='symboltd>" + json[i].action + "</td>");
             $('#parseData').append(tr);
         }
     }
