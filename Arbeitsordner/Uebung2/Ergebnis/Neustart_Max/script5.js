cbd1c8e20d63ba57429fac0204303a8b596f34ce
Philipp W <waackphilipp@gmail.com>
2014-11-24 01:48:52 +0100

    änderungen usw..

--- Arbeitsordner/Uebung2/Ergebnis/Neustart_Max/script5.js
+++ Arbeitsordner/Uebung2/Ergebnis/Neustart_Max/script5.js
@@ -1,17 +1,19 @@
 var getName = $("#yourName");
 var getMail = $("#yourEmail");
 var getQuota = $("#yourQuota");
+var getUserName = $("#userName1");
 
 $.ajax( {
 type:'Get',
 url:'http://wme.lehre.imld.de:8080/wme14-15/api/users',
 success:function(data) {
     var users = data.getElementsByTagName("user");
-    var name = users[2].getElementsByTagName("display_name");
-    var mail = users[2].getElementsByTagName("email_address");
-    var quota = users[2].getElementsByTagName("quota");
-    var quotaUsed = users[2].getElementsByTagName("quota_used");
-
+    var userName1 = users[3].getElementsByTagName("username");
+    var name = users[3].getElementsByTagName("display_name");
+    var mail = users[3].getElementsByTagName("email_address");
+    var quota = users[3].getElementsByTagName("quota");
+    var quotaUsed = users[3].getElementsByTagName("quota_used");
+    getUserName.append(""+userName1[0].innerHTML);
     getName.append(""+name[0].innerHTML);
     getMail.append(""+mail[0].innerHTML);
     getQuota.append(""+quota[0].innerHTML + " / " + quotaUsed[0].innerHTML);
